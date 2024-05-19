237ef184ad9eac5412f8be2857cac0273ed1b37e
frfoc1 <frfoc1@tchaikovsky.gel.ulaval.ca>
2024-05-19 16:36:43 -0400

    mp4 maker

--- lantern_scripts/mp4_maker.py
+++ lantern_scripts/mp4_maker.py
@@ -5,6 +5,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make a GIF from input images')
     parser.add_argument("--input_Addr", type=str, help='input folder')
+    parser.add_argument("--out_file", type=str, help='out file')
     # parser.add_argument("--input_pano_Addr", type=str, help='input folder')
     args = parser.parse_args()
 
@@ -30,7 +31,7 @@ if __name__ == '__main__':
         # img[0:small_pano.shape[0], 0:small_pano.shape[1]] = small_pano
         images.append(img)
         
-    out = cv2.VideoWriter('saussan_ldr_light.mp4', cv2.VideoWriter_fourcc(*'FMP4'), 10, size)
+    out = cv2.VideoWriter(args.out_file, cv2.VideoWriter_fourcc(*'FMP4'), 10, size)
     # out = cv2.VideoWriter('my_apartement_render.mp4', cv2.VideoWriter_fourcc('X','V','I','D'), 15, size)
 
     for i in range(len(images)):
